fe3cf101cc7dc14219578d57fb5ee03ca1f94bf1
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2020-12-16 22:34:48 -0500

    first commit

---
+++ stanza_template.py
@@ -0,0 +1,7 @@
+import stanza
+
+#stanza.download('en') # download English model
+nlp = stanza.Pipeline('en') # initialize English neural pipeline
+doc = nlp("Barack Obama was born in Hawaii.") # run annotation over a sentence
+
+print(doc)
